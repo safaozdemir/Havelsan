ba818002cd3ec74c604b388e27d62a92a27836ed
safaozdemir <sozdemir4444@hotmail.com>
2022-12-16 16:55:26 +0300

    add while loop

--- Havelsan.playground/Contents.swift
+++ Havelsan.playground/Contents.swift
@@ -4,5 +4,9 @@ var greeting = "Hello, welcome swift world"
 print(greeting)
 
 for i in 1...5 {
-	print("New Pull Request)
+	print("New Pull Request")
+	while (i<3){
+		print("New Pull Request")
+		i=i+1
+	}
 }
